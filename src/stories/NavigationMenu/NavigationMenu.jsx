5a1258d1953118b548d7b6261228209e39313311
willy <ws.blotocol@gmail.com>
2024-07-12 05:11:58 +0800

    Latest stable with bootstrap components

--- src/stories/NavigationMenu/NavigationMenu.jsx
+++ src/stories/NavigationMenu/NavigationMenu.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import "./navigationmenu.scss";
 import { Button } from "stories/Button/Button";
 import { getKeyDex } from "util/common";
+import { Collapse, Fade } from "react-bootstrap";
 
 const VARIANT = {
     header: "header-menu",
@@ -13,7 +14,7 @@ export const NavigationMenu = ({ variant, items, itemComponent: ItemComponent, a
     const [showFlyout, setShowFlyout] = React.useState();
     const [selectMenu, setSelectMenu] = React.useState();
     const defaultClass = VARIANT[variant || "header"];
-    const navClasses = [defaultClass, showFlyout && variant === "header" ? "show-flyout" : ""].join(" ");
+    const navClasses = [defaultClass, variant === "header" ? "mobile-flyout" : ""].join(" ");
 
     useEffect(() => {
         setSelectMenu(activeMenu);
@@ -21,36 +22,38 @@ export const NavigationMenu = ({ variant, items, itemComponent: ItemComponent, a
     }, [activeMenu]);
     return (
         <>
-            <nav className={navClasses}>
-                {variant === "header" && (
-                    <div className="btn-close">
-                        <Button className="btn-wrapper" onClick={() => setShowFlyout(false)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21">
-                                <g fill="#D0D6F9" fill-rule="evenodd">
-                                    <path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z" />
-                                    <path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z" />
-                                </g>
-                            </svg>
-                        </Button>
-                    </div>
-                )}
-                <ul className="nav-menu mb-0">
-                    {items.map((item) => (
-                        <ItemComponent
-                            key={getKeyDex()}
-                            active={selectMenu === item.id}
-                            onClick={(selectedValue) => {
-                                setSelectMenu(selectedValue);
-                                onSelect(selectedValue);
-                            }}
-                            {...item}
-                        />
-                    ))}
-                </ul>
-            </nav>
+            <Fade in={showFlyout} timeout={10000}>
+                <nav className={navClasses}>
+                    {variant === "header" && (
+                        <div className="btn-close">
+                            <Button className="btn-wrapper" aria-expanded={showFlyout} aria-controls="mobile-flyout" onClick={() => setShowFlyout(false)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21">
+                                    <g fill="#D0D6F9" fill-rule="evenodd">
+                                        <path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z" />
+                                        <path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z" />
+                                    </g>
+                                </svg>
+                            </Button>
+                        </div>
+                    )}
+                    <ul className="nav-menu mb-0">
+                        {items.map((item) => (
+                            <ItemComponent
+                                key={getKeyDex()}
+                                active={selectMenu === item.id}
+                                onClick={(selectedValue) => {
+                                    setSelectMenu(selectedValue);
+                                    onSelect(selectedValue);
+                                }}
+                                {...item}
+                            />
+                        ))}
+                    </ul>
+                </nav>
+            </Fade>
             {variant === "header" && (
                 <div className="nav-mobile-hamburger">
-                    <Button className="btn-wrapper" onClick={() => setShowFlyout(true)}>
+                    <Button className="btn-wrapper" aria-expanded={showFlyout} aria-controls="mobile-flyout" onClick={() => setShowFlyout(true)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21">
                             <g fill="#D0D6F9" fill-rule="evenodd">
                                 <path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" />
